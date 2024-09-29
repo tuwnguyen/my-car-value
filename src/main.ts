fb10293c45d662a5457c3ef9bf7cbdc73653c3e8
tuwnguyen <tuwnguyen@gmail.com>
2024-09-29 12:28:03 +0700

    feat: added pipe, dto and route for auth createUser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // whitelist: true strip out all property in the body not in dto
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
