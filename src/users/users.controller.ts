cb50c4f7ec73b60344a400a82b1d5e22f098124a
tuwnguyen <tuwnguyen@gmail.com>
2024-09-29 16:31:31 +0700

    feat: Imp CRUD User data, added hook decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,47 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.userService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @Get('')
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.remove(id);
+  }
+
+  @Patch('/:id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateUserDto,
+  ) {
+    return this.userService.update(id, body);
   }
 }
