ed15686b94172c37b6d2988afdf7e99fcbab2d82
tuwnguyen <tuwnguyen@gmail.com>
2024-10-08 22:08:30 +0700

    test: Write follow signup and get current user

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -27,4 +27,20 @@ describe('Auth System', () => {
         expect(email).toEqual(emailRes);
       });
   });
+
+  it('signup a new user then get the currently logged in user', async () => {
+    const email = 'asdf@gmail.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'pass' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+    expect(body.email).toEqual(email);
+  });
 });
