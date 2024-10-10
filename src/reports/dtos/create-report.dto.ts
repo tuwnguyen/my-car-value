89bcdc38216c8bc09c6e755be25d9ff9392d6bc0
tuwnguyen <tuwnguyen@gmail.com>
2024-10-10 18:55:04 +0700

    feat: imp creat report model

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,38 @@
+import {
+  IsString,
+  IsNumber,
+  Min,
+  Max,
+  IsLongitude,
+  IsLatitude,
+  min,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(100000)
+  mileage: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+}
