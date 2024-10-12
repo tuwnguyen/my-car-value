445f8d613dbf826c25a0a9c33a0b296d1afbac2d
tuwnguyen <tuwnguyen@gmail.com>
2024-10-12 16:30:35 +0700

    feat: imp query builder in TypORM to select estimate price of query reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -42,6 +42,6 @@ export class ReportsController {
 
   @Get()
   getEstimate(@Query() query: GetEstimateDto) {
-    console.log(query);
+    return this.reportsService.createEstimate(query);
   }
 }
