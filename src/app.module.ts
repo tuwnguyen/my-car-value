f8ee1c5f2c11885dd4c3dcfa679ef7b92635bdf2
tuwnguyen <tuwnguyen@gmail.com>
2024-10-12 19:18:48 +0700

    deployment: setup cookie-key environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,11 +42,13 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['dddddd'],
+          keys: [this.configService.get<string>('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
